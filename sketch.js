5d47232d2ba9a541eefdf69f791381bca0d58a3f
charlesz11 <122957553+charlesz11@users.noreply.github.com>
2023-01-20 10:49:57 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 function setup() {
-	createCanvas(800, 800); // make an HTML canvas element width x height pixels
+	createCanvas(1900, 600); // make an HTML canvas element width x height pixels
 
 }
 
 function draw() {
 	background(200);
-	initBox()
+	// initBox()
 	initDate()
 }
 
@@ -26,53 +26,69 @@ function initBox() {
 
 }
 
-// 绘制文本内容
-function initDate() {
-	// 文字边框宽度  文字边框颜色
-	strokeWeight(14)
-	stroke(10, 10, 10)
-	// 文字大小
-	textSize(50);
-	// 文字填充
-	fill(43, 109, 203);
-	// 当时间小于10 是一个数字时在前面加上0
-	if (hour() < 10) {
-		// 显示文本内容，x坐标  Y坐标
-		text('0' + hour(), 175, 287);
-	} else {
-		text(hour(), 175, 287);
-	}
-	text(":", 245, 280)
-	// 文字填充颜色
-	fill(80, 152, 56);
-	if (minute() < 10) {
-		text('0' + minute(), 270, 285);
-	} else {
-		text(minute(), 270, 285);
-	}
-	text(":", 339, 280)
-	// 文字填充颜色
-	fill(255, 245, 245);
-	if (second() < 10) {
-		if (second() < 10) {
-			statuss = true
-		}
-		text('0' + second(), 360, 285);
-	} else {
-		if (second() >= 59 && statuss) {
-			statuss = false
-			if (minute() + 1 > 59) {
-				console.log('current minutes is ', '01')
-			} else {
-				let minutes = minute() + 1
-				if (minutes < 10) {
-					console.log('current minutes is ', '0' + minutes)
-				} else {
-					console.log('current minutes is ', minutes)
-				}
 
-			}
-		}
-		text(second(), 360, 285);
+// 绘制水漏时钟
+function initDate() {
+	let H = hour()
+	let M = minute()
+	let S = second()
+
+
+
+	// 时
+	noStroke()
+	fill(255, 255, 255)
+	quad(100, 500, 200, 500, 200, 350, 100, 350);
+
+	fill(0, 194, 151)
+	quad(100, 500, 200, 500, 200, 500 - (H * 6.25), 100, 500 - (H * 6.25));
+
+
+	// 链接器
+	fill(255, 255, 255)
+	strokeWeight(4)
+	stroke(85, 85, 79)
+	quad(200, 380, 300, 380, 300, 370, 200, 370);
+
+
+	// 分
+	noStroke()
+	fill(255, 255, 255)
+	quad(300, 400, 400, 400, 400, 250, 300, 250);
+	fill(44, 153, 58)
+	quad(300, 400, 400, 400, 400, 400 - (M * 2.5), 300, 400 - (M * 2.5));
+
+
+	// 链接器
+	strokeWeight(4)
+	stroke(85, 85, 79)
+	fill(255, 255, 255)
+	quad(400, 260, 500, 260, 500, 270, 400, 270);
+
+
+	// 秒
+	noStroke()
+	fill(255, 255, 255)
+	quad(500, 300, 600, 300, 600, 150, 500, 150);
+	fill(33, 160, 255)
+	quad(500, 300, 600, 300, 600, 150 + (S * 2.5), 500, 150 + (S * 2.5));
+
+
+
+
+	// 打印时间
+	if (S < 10) {
+		statuss = true
 	}
+	if (S >= 59 && statuss) {
+		statuss = false
+		if (M + 1 >= 60) {
+			console.log('当前分钟', M + 1 - 60)
+
+		} else {
+			console.log('当前分钟', M + 1)
+		}
+
+	}
+
 }
